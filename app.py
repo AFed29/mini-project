c740e209678fb2eb83cc2826093adf378ff601b5
Alex Fedenczuk <alex.fedenczuk@accenture.com>
2022-02-04 13:16:55 +0000

    added start of orders functionality

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from typing import List
 
+orders = []
+
 main_menu_text = """
     MAIN MENU
     
     0. Exit App
     1. Products Menu
     2. Couriers Menu
+    3. Orders Menu
 """
 products_menu_text = """
     PRODUCTS MENU
@@ -23,6 +26,15 @@ couriers_menu_text = """
     0. Return to main menu
     1. Show all couriers
     2. Create new courier
+"""
+
+orders_menu_text = """
+    ORDERS MENU
+    
+    0. Return to main menu
+    1. Show all Orders
+    2. Create new order
+    3. Update order status
 """
 
 def get_file_contents(file_name: str) -> List[str]:
@@ -56,18 +68,22 @@ while True:
     # Enter Products Menu
     if main_menu_input == 1:
         while True:
+            # Display products menu and take option
             print(products_menu_text)
             products_menu_input = int(input("Please make a choice from above: "))
             
+            # Return to main menu
             if products_menu_input == 0:
                 break
             
+            # Display list of products
             if products_menu_input == 1:
                 print("Products List:")
                 
                 products = get_file_contents('products.txt')
                 print(format_list_for_display(products))     
             
+            # Add new product
             elif products_menu_input == 2:  
                 new_product_input = input("Please enter a new product: ")
                 with open('products.txt', 'a') as open_file:
@@ -101,19 +117,61 @@ while True:
     # Enter Couriers Menu 
     if main_menu_input == 2:
         while True:
+            # Display couriers menu and take option
             print(couriers_menu_text)
             couriers_menu_input = int(input("Please make a choice from above: "))
             
+            # Return to main menu
             if couriers_menu_input == 0:
                 break
             
+            # Display list of couriers
             if couriers_menu_input == 1:
                 print("Couriers List:")
                 
                 couriers = get_file_contents('couriers.txt')
                 print(format_list_for_display(couriers))  
             
+            # Add new courier
             elif couriers_menu_input == 2:  
                 new_courier_input = input("Please enter a new courier: ")
                 with open('couriers.txt', 'a') as open_file:
                     open_file.write(new_courier_input + '\n')
+                    
+    # Enter orders menu
+    if main_menu_input == 3:
+        # Display orders menu and take option
+        while True:
+            print(orders_menu_text)
+            orders_menu_input = int(input("Please make a choice from above: "))
+            
+            # Return to main menu
+            if orders_menu_input == 0:
+                break
+            
+            # Display list of orders
+            if orders_menu_input == 1:
+                print("orders List:")
+                print(orders)  
+            
+            # Add new order
+            elif orders_menu_input == 2:  
+                customer_name = input("Please enter a customer name: ")
+                customer_address = input("Please enter a customer address: ")
+                customer_phone = input("Please enter a customer phone number: ")
+                
+                print("Couriers List:")
+                couriers = get_file_contents('couriers.txt')
+                print(format_list_for_display(couriers))
+                
+                courier = int(input("Please enter a courier number: "))
+                
+                orders.append({
+                    "customer_name": customer_name,
+                    "customer_address": customer_address,
+                    "customer_phone": customer_phone,
+                    "courier": courier,
+                    "status": "preparing"
+                })
+            
+
